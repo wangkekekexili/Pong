f459df5b83eb533e978fe11b970c22ac4ea33896
Ke Wang <wangkekekexili@gmail.com>
2018-12-29 10:16:05 -0500

    Score on side wall collision.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static int player1Score;
+    private static int player2Score;
+    public static int Player1Score
+    {
+        get
+        {
+            return player1Score;
+        }
+    }
+    public static int Player2Score
+    {
+        get
+        {
+            return player2Score;
+        }
+    }
+
+    public static void Score(string wall)
+    {
+        switch (wall)
+        {
+            case "LeftWall":
+                player2Score += 1;
+                break;
+            case "RightWall":
+                player1Score += 1;
+                break;
+        }
+        Debug.Log("player1 score " + player1Score);
+        Debug.Log("player2 score " + player2Score);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width/2-2, 20, 100, 100), $"{player1Score}:{player2Score}");
+    }
+}
---
+++ Assets/Scripts/SideWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name != "Ball")
+        {
+            return;
+        }
+        var wallName = GetComponent<Transform>().name;
+        GameManager.Score(wallName);
+    }
+}
