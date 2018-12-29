68678ce34ee1e421034cb923b47eb79d42d2b60b
Ke Wang <wangkekekexili@gmail.com>
2018-12-29 09:37:02 -0500

    Make ball move.

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        Random.InitState(System.DateTimeOffset.Now.Millisecond);
+        var randomNumber = Random.Range(0f, 1f);
+        if (randomNumber <= 0.5)
+        {
+            Debug.Log("right");
+            _rigidbody2D.AddForce(new Vector2(80, 10));
+        }
+        else
+        {
+            Debug.Log("left");
+            _rigidbody2D.AddForce(new Vector2(-80, -10));
+        }
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            //var velY = _rigidbody2D.velocity.y;
+            //velY = velY / 2 + collision.collider.attachedRigidbody.velocity.y / 3;
+            //_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, velY);
+        }
+    }
+}
